1d68dabcda50164928436490f77a75d2434933a4
Stewart Knapman <info@stewartknapman.com>
2015-06-17 14:54:33 +1200

    fix linked option selectors not firing

--- src/js/bsify.linked_product_options.js
+++ src/js/bsify.linked_product_options.js
@@ -11,16 +11,16 @@ LinkedProductOptions.updateOptionsInSelector = function(selectorIndex) {
   switch (selectorIndex) {
     case 0:
       key = 'root';
-      selector = jQuery('.single-option-selector:eq(0)');
+      selector = $('.single-option-selector:eq(0)');
       break;
     case 1:
-      key = jQuery('.single-option-selector:eq(0)').val();
-      selector = jQuery('.single-option-selector:eq(1)');
+      key = $('.single-option-selector:eq(0)').val();
+      selector = $('.single-option-selector:eq(1)');
       break;
     case 2:
-      key = jQuery('.single-option-selector:eq(0)').val();  
-      key += ' / ' + jQuery('.single-option-selector:eq(1)').val();
-      selector = jQuery('.single-option-selector:eq(2)');
+      key = $('.single-option-selector:eq(0)').val();  
+      key += ' / ' + $('.single-option-selector:eq(1)').val();
+      selector = $('.single-option-selector:eq(2)');
   }
   
   var initialValue = selector.val();
@@ -29,18 +29,18 @@ LinkedProductOptions.updateOptionsInSelector = function(selectorIndex) {
   if (availableOptions !== undefined) {
     for (var i=0; i<availableOptions.length; i++) {
       var option = availableOptions[i];
-      var newOption = jQuery('<option></option>').val(option).html(option);
+      var newOption = $('<option></option>').val(option).html(option);
       selector.append(newOption);
     }
-    jQuery('.swatch[data-option-index="' + selectorIndex + '"] .swatch-element').each(function() {
-      if (jQuery.inArray($(this).attr('data-value'), availableOptions) !== -1) {
+    $('.swatch[data-option-index="' + selectorIndex + '"] .swatch-element').each(function() {
+      if ($.inArray($(this).attr('data-value'), availableOptions) !== -1) {
         $(this).removeClass('soldout').show().find(':radio').removeAttr('disabled','disabled').removeAttr('checked');
       }
       else {
         $(this).addClass('soldout').hide().find(':radio').removeAttr('checked').attr('disabled','disabled');
       }
     });
-    if (jQuery.inArray(initialValue, availableOptions) !== -1) {
+    if ($.inArray(initialValue, availableOptions) !== -1) {
       selector.val(initialValue);
     }
     selector.trigger('change');
@@ -78,17 +78,16 @@ LinkedProductOptions.linkOptionSelectors = function(product) {
   if (product.options.length > 1) LinkedProductOptions.updateOptionsInSelector(1);
   if (product.options.length === 3) LinkedProductOptions.updateOptionsInSelector(2);
   // When there is an update in the first dropdown.
-  jQuery(".single-option-selector:eq(0)").change(function() {
+  $(document).on('change', '.single-option-selector:eq(0)', function() {
     LinkedProductOptions.updateOptionsInSelector(1);
     if (product.options.length === 3) LinkedProductOptions.updateOptionsInSelector(2);
     return true;
   });
   // When there is an update in the second dropdown.
-  jQuery(".single-option-selector:eq(1)").change(function() {
+  $(document).on('change', '.single-option-selector:eq(1)', function() {
     if (product.options.length === 3) LinkedProductOptions.updateOptionsInSelector(2);
     return true;
   });
-  
 };
 
 module.exports = LinkedProductOptions;
