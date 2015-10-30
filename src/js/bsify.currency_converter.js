20d8fb7a6946d31e67022def20c88b03df2ff100
Stewart Knapman <info@stewartknapman.com>
2015-10-30 17:03:20 +1300

    convering js

--- src/js/bsify.currency_converter.js
+++ src/js/bsify.currency_converter.js
@@ -1,10 +1,32 @@
 var Cookies = require('./_cookies.js');
 var CurrencyConverter = {};
 
+var currency_switcher_selector = '[data-currency-code]';
+var currency_current_selector = '[data-currency-current]';
+
 CurrencyConverter.init = function () {
   if (Bsify.currency_converter_options.enabled) {
-    console.log('CurrencyConverter', Bsify.currency_converter_options);
+    
+    // check for currency cookie. If we don't have one set it to be the stores currency
+    if (!Cookies.hasItem('current_currency')) {
+      Cookies.setItem('current_currency', Bsify.currency_converter_options.store_currency);
+    }
+    
+    // add listener for conveter
+    $(document).on('click', currency_switcher_selector, function (e) {
+      e.preventDefault();
+      CurrencyConverter.convert();
+    });
+    
+    // convert the currency
+    CurrencyConverter.convert();
+    
+    console.log('CurrencyConverter', Bsify.currency_converter_options, Cookies.getItem('current_currency'));
   }
+};
+
+CurrencyConverter.convert = function () {
+  console.log('CurrencyConverter SWITCH', Cookies.getItem('current_currency'));
 };
 
 module.exports = CurrencyConverter;
